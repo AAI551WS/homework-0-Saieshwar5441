bdc357b8da3ef4ee45be012290c0e3b9798f198c
Saieshwar5441 <153687173+Saieshwar5441@users.noreply.github.com>
2024-01-31 17:03:54 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -2,16 +2,16 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+    The function prints "Hello, World!"
     """
 
     # Write a comment; comments are very useful to other programmers.
-
+    # The function is used to print hello world
     # Write a Python statement to print Hello, World!
-
+    print ("Hello, World!")
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
